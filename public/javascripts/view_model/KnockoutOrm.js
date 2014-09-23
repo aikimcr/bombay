53333b0aaca74e924caa90383cae2345ddeba762
Michael Riehle <mriehle@mriehle.com>
2014-09-23 10:30:17 -0700

    Checkpoint.  The code doesn't actually work, yet, but there are tests and a strategy developing.  Time to make sure current work is not lost.

---
+++ public/javascripts/view_model/KnockoutOrm.js
@@ -0,0 +1,171 @@
+function orm() { }
+
+// Table Management
+orm.table = function(context_base, table_name, model_key, url, columns) {
+  this.context_base = context_base;
+  this.table_name = table_name;
+  this.model_key = model_key;
+  this.url = url;
+  this.columns = columns;
+  this.list = context_base[model_key + 's'];
+};
+
+orm.table.prototype.create = function(data, callback) {
+  var url = this.url;
+  var svc = service.getInstance();
+  svc.post(url, function(result_code, result) {
+    if (result_code == 200) {
+      var model = result[this.model_key];
+      var row = new orm.table.row(this, model);
+
+      try {
+        this.list.insert(row);
+      } catch (err) {
+        return callback(err, row);
+      };
+
+      callback(null, row);
+    } else if (result_code == 304) {
+      var row = this.list.get(result[this.model_key].id);
+      callback(null, row);
+    } else {
+      callback(result_code, result);
+    }
+  }.bind(this), data);
+};
+
+orm.table.prototype.delete = function(row, confirm_callback, callback, opt_event) {
+  confirm_callback(this, row, opt_event, function(delete_it) {
+    if (delete_it) {
+      var svc = service.getInstance();
+      var url = this.url + '?id=' + row.id();
+      svc.delete(url, function(result_code, result) {
+        if (result_code == 200) {
+          var list_key = this.list_key;
+          var model_key = this.model_key;
+          this.list.delete(row);
+          if (callback) callback(null, result);
+        } else if (result_code == 304) {
+          if (callback) callback(new Error('Already Deleted', result));
+        } else {
+          if (callback) callback(result_code, result);
+        }
+      }.bind(this));
+    } else {
+      if (callback) callback(new Error('Not Deleted'));
+    }
+  });
+};
+
+orm.table.prototype.get = function(id, callback) {
+  var svc = service.getInstance();
+  var url = this.url + '?id=' + id;
+  svc.get(url, function(result_code, result) {
+    if (result_code == 200 || result_code == 304) {
+      var model =  result[this.model_key];
+      var row = new orm.table.row(this, model);
+      callback(null, row);
+    } else {
+      callback(result_code);
+    }
+  }.bind(this));
+};
+
+orm.table.prototype.load = function(callback) {
+  var svc = service.getInstance();
+  svc.get(this.url, function(result_code, result) {
+    if (result_code == 200) {
+      this.list.clear();
+      var model_key = 'all_' + this.model_key + 's';
+
+      if (result[model_key]) {
+        var list_model = [];
+        result[model_key].forEach(function(model) {
+          list_model.push(new orm.table.row(this, model));
+        }.bind(this));
+        this.list.set(list_model);
+        callback(null, this.list);
+      } else {
+        callback(new Error('no_result', {model_key: this.model_key, error: result.toString()}))
+      }
+    } else if (result_code = 304) {
+      callback(null, this.list);
+    } else {
+      callback(result_code, result);
+    }
+  }.bind(this));
+};
+
+// View Model Row.
+orm.table.row = function(table, model) {
+  this.table = table;
+  this.id = ko.observable(model.id);
+
+  Object.keys(this.table.columns).forEach(function(column_name) {
+    var column_def = this.table.columns[column_name];
+    this[column_name] = ko.observable(model[column_name]);
+  }.bind(this));
+};
+
+// List management.
+// This should all be synchronous.
+orm.table.list = function(table_name) {
+  this.table_name = ko.observable(table_name);
+  this.list = ko.observableArray([]);
+};
+
+orm.table.list.prototype.length = function() {
+  return this.list().length;
+};
+
+orm.table.list.prototype.get = function(id) {
+  return ko.utils.arrayFirst(this.list(), function(item) {
+    return item.id() == id;
+  }.bind(this));
+};
+
+orm.table.list.prototype.set = function(list_model) {
+  this.list(list_model);
+};
+
+orm.table.list.prototype.clear = function() {
+  this.list.removeAll();
+};
+
+orm.table.list.prototype.insert = function(row, clobber) {
+  if (this.get(row.id())) {
+    if (clobber) {
+      this.delete(row.id());
+    } else {
+      throw new Error('Row already exists');
+    }
+  }
+
+  this.list.push(row);
+};
+
+orm.table.list.prototype.delete = function(id) {
+  var rows = this.list.remove(function(row) {
+    return row.id() == id;
+  });
+
+  if (rows.length > 1) {
+    throw new Error('delete should remove zero or one row, not ' + rows.length);
+  }
+
+  return rows[0];
+};
+
+// The main entry point for defining a view model.
+orm.define = function(context_base, table_name, definition, options) {
+  var model_key = options['model_key'] || table_name;
+  var url = './' + options['url'] || table_name;
+  var columns = {};
+
+  Object.keys(definition).forEach(function(column_name) {
+    columns[column_name] = definition[column_name];
+  });
+
+  context_base[model_key + 's'] = new orm.table.list(table_name);
+  return new orm.table(context_base, table_name, model_key, url, columns);
+};
---
+++ public/test/KnockoutOrm.js
@@ -0,0 +1,331 @@
+function TestContext() {
+}
+
+describe('KnockoutOrm Object Definitions', function() {
+  describe('define an orm.table.list', function() {
+    it('should create the object without errors', function(done) {
+      var list = new orm.table.list('Oxbow');
+      should.exist(list);
+      list.should.have.property('table_name');
+      ko.isObservable(list.table_name).should.be.true;
+      list.table_name().should.equal('Oxbow');
+      list.should.have.property('list');
+      ko.isObservable(list.list).should.be.true;
+      list.list().should.eql([]);
+      done();
+    });
+  });
+
+  describe('define an orm.table directly', function() {
+    var test_context;
+
+    var table_name = 'Oxbow';
+    var list_name = table_name + 's';
+    var columns = {
+      trout: { type: 'string' },
+      cichlid: { type: 'integer' }
+    };
+
+    before(function(done) {
+      test_context = new TestContext();
+      test_context[list_name] = new orm.table.list(table_name);
+      done();
+    });
+
+    after(function(done) {
+      test_context = null;
+      done();
+    });
+
+    it('should create the table object without error', function(done) {
+      var table = new orm.table(
+        test_context,
+        table_name,
+        'Lake',
+        './amazon',
+        columns
+      );
+      should.exist(table);
+      table.should.have.property('table_name', table_name);
+      table.should.have.property('model_key', 'Lake');
+      table.should.have.property('url', './amazon');
+      table.should.have.property('columns');
+      table.columns.should.eql(columns);
+      done();
+    });
+  });
+
+  describe('define an orm.table.row directly', function() {
+    var test_context;
+    var table;
+
+    var table_name = 'Oxbow';
+    var list_name = table_name + 's';
+    var columns = {
+      trout: { type: 'string' },
+      cichlid: { type: 'integer' }
+    };
+
+    before(function(done) {
+      test_context = new TestContext();
+      test_context[list_name] = new orm.table.list(table_name);
+      done();
+    });
+
+    before(function(done) {
+      table = new orm.table(
+        test_context,
+        table_name,
+        table_name,
+        './' + table_name,
+        columns
+      );
+      done();
+    });
+
+    after(function(done) {
+      test_context = null;
+      table = null;
+      done();
+    });
+
+    it('should create a valid table row without an error', function(done) {
+      var row = new orm.table.row(table, {id: 1, trout: 'salmon', cichlid: 27});
+
+      should.exist(row);
+
+      row.should.have.property('id');
+      ko.isObservable(row.id).should.be.true;
+      row.id().should.equal(1);
+
+      row.should.have.property('trout');
+      ko.isObservable(row.trout).should.be.true;
+      row.trout().should.equal('salmon');
+
+      row.should.have.property('cichlid');
+      ko.isObservable(row.cichlid).should.be.true;
+      row.cichlid().should.equal(27);
+
+      done();
+    });
+  });
+});
+
+describe('KnockoutOrm list management', function() {
+  var list;
+  var start_list = [];
+  var list_names = ['platypus', 'angel', 'danio', 'discus', 'wrasse', 'tetra'];
+
+  before(function(done) {
+    list = new orm.table.list('Oxbow');
+    done();
+  });
+
+  before(function(done) {
+    for(var i=1;i<=5;i++) {
+      start_list.push({
+        id: ko.observable(i),
+        name: ko.observable(list_names[i])
+      });
+    }
+    done();
+  });
+
+  after(function(done) {
+    list = null;
+    done();
+  });
+
+  it('should fill in the list', function(done) {
+    list.length().should.equal(0);
+    var error = list.set(start_list);
+
+    should.not.exist(error);
+    list.length().should.equal(5);
+
+    list.list().forEach(function(row, index) {
+      row.should.have.property('id');
+      row.id().should.equal(index + 1);
+    });
+
+    done();
+  });
+
+  it('should get the row', function(done) {
+    var row;
+    
+    (function() {
+      row = list.get(2)
+    }).should.not.throw();
+
+    should.exist(row);
+    row.id().should.equal(2);
+    row.name().should.equal('danio');
+    done();
+  });
+
+  it('should delete the row', function(done) {
+    var row;
+
+    (function() {
+      row = list.delete(3);
+    }).should.not.throw();
+
+    should.exist(row);
+    row.id().should.equal(3);
+    row.name().should.equal('discus');
+
+    list.length().should.equal(4);
+    done();
+  });
+
+  it('should not get the row', function(done) {
+    var row;
+    
+    (function() {
+      row = list.get(3)
+    }).should.not.throw();
+
+    should.not.exist(row);
+    done();
+  });
+
+  it('should insert a row', function(done) {
+    var row = {
+      id: ko.observable(20),
+      name: ko.observable('loach')
+    };
+
+    (function() {
+      list.insert(row, false);
+    }).should.not.throw();
+
+    var new_row = list.get(20);
+
+    should.exist(new_row);
+    new_row.id().should.equal(20);
+    new_row.name().should.equal('loach');
+
+    list.length().should.equal(5);
+    done();
+  });
+
+  it('should clobber the row', function(done) {
+    var row = {
+      id: ko.observable(20),
+      name: ko.observable('goby')
+    };
+
+    (function() {
+      list.insert(row, true);
+    }).should.not.throw();
+
+    var new_row = list.get(20);
+
+    should.exist(new_row);
+    new_row.id().should.equal(20);
+    new_row.name().should.equal('goby');
+
+    list.length().should.equal(5);
+    done();
+  });
+
+  it('should get an error', function(done) {
+    var row = {
+      id: ko.observable(20),
+      name: ko.observable('loach')
+    };
+
+    (function() {
+      list.insert(row, false);
+    }).should.throw();
+
+    var new_row = list.get(20);
+
+    should.exist(new_row);
+    new_row.id().should.equal(20);
+    new_row.name().should.equal('goby');
+
+    list.length().should.equal(5);
+    done();
+  });
+});
+
+describe.only('Table Management', function() {
+  var instance_stub;
+  var test_context;
+  var table;
+
+  var table_name = 'oxbow';
+  var list_name = table_name + 's';
+  var columns = {
+    species: { type: 'string' },
+    count: { type: 'integer' }
+  };
+
+  before(function(done) {
+    test_context = new TestContext();
+    test_context[list_name] = new orm.table.list(table_name);
+    done();
+  });
+
+  before(function(done) {
+    table = new orm.table(
+      test_context,
+      table_name,
+      table_name,
+      './' + table_name,
+      columns
+    );
+    done();
+  });
+  
+  before(function(done) {
+    var stub_service = {
+      post: function(url, callback, data) {
+        url.should.equal('./' + table_name);
+
+        var req_body = {};
+        Object.keys(data).forEach(function(column_name) {
+          column_name.should.not.equal('description');
+          ko.isObservable(data[column_name]).should.be.false;
+          req_body[column_name] = data[column_name];
+        });
+        req_body.id = 1;
+        var req_model = {};
+        req_model[table_name] = req_body;
+        callback(200, req_model);
+      },
+      delete: function(url, callback) {
+      },
+      get: function(url, callback) {
+      }
+    };
+    instance_stub = sinon.stub(service, 'getInstance', function() { return stub_service; });
+    done();
+  });
+
+  after(function(done) {
+    test_context = null;
+    table = null;
+    done();
+  });
+
+  it('should call the create api', function(done) {
+    table.create({
+      id: ko.observable(1),
+      species: ko.observable('trout'),
+      count: ko.observable(540),
+      description: ko.observable('Trout are plentiful')
+    }, function(result_code, result) {
+      should.not.exist(result_code);
+      should.exist(result);
+      result.should.eql({
+        id: 1,
+        species: 'trout',
+        count: 540
+      });
+      done();
+    });
+  });
+});
