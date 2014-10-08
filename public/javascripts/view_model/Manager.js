2359d6a12eecc5bbc84985c91b966fbad0206df7
Michael Riehle <mriehle@mriehle.com>
2014-10-08 08:39:28 -0700

    Band tab is mostly right.  Checkpoint the code.

--- public/javascripts/view_model/Manager.js
+++ public/javascripts/view_model/Manager.js
@@ -117,18 +117,94 @@ function Manager(for_test) {
     { value: 'G#m', value_text: 'G#m' }
   ];
 
-  this.bands = new BandList();
-  this.persons = new PersonList();
-  this.artists = new ArtistList();
-  this.songs = new SongList();
-  this.band_members = new BandMemberList();
-  this.band_songs = new BandSongList();
-  this.song_ratings = new SongRatingList();
-  this.requests = new RequestList();
+  this.createBandTable();
+  this.createPersonTable();
+  var artist = orm.define(this, 'artist', {
+    name: {type: 'string'}
+  });
+  var song = orm.define(this, 'song', {
+    name: {type: 'string'},
+    artist_id: {type: 'reference', reference_table: artist},
+    key_signature: {type: 'string'}
+  }):
+  var band_member = orm.define(this, 'band_member', {
+    band_id: {type: 'reference', reference_table: band},
+    person_id: {type: 'reference', reference_table: person},
+    band_admin: {type: 'boolean'}
+  });
+  var band_song = orm.define(this, 'band_song', {
+    band_id: {type: 'reference', reference_table: band},
+    song_id: {type: 'reference', reference_table: song},
+    key_signature: {type: 'string'},
+    primary_vocal_id: {type: 'reference', reference_table: band_member},
+    secondary_vocal_id: {type: 'reference', reference_table: band_member}
+  });
+  var song_rating = orm.define(this, 'song_rating', {
+    band_member_id: {type: 'reference', reference_table: band_member},
+    band_song_id: {type: 'reference', reference_table: band_song},
+    rating: {type: 'integer'},
+    is_new: {thpe: 'boolean'},
+  });
+  var request = orm.define(this, 'request', {
+    request_type: {type: 'integer'},
+    timestamp: {type: 'date'},
+    person_id: {type: 'reference', reference_table: person},
+    band_id: {type: 'reference', reference_table: band},
+    description: {type: 'string'},
+    status: {type: 'integer'}
+  });
+
   this.reports = new ReportList();
 
-  this.current_person = ko.observable(new Person(-1, '', '', '', '', false));
-  this.current_band = ko.observable(new Band(-1, ''));
+  this.current_person = ko.observable();
+  this.current_band = ko.observable();
+
+  band.load(function(err, result) {
+    if (err) throw err;
+
+    person.load(function(err, result) {
+      if (err) throw err;
+
+      var svc = service.getInstance();
+      svc.get('./session_info', function(result_code, result) {
+        if (result_code != 200 && result_code != 304) {
+          throw new Error('Unexpected result ' + result_code);
+        }
+        
+        this.current_person(person.get(result.person_id()));
+      }.bind(this));
+
+      band_member.load(function(err, result) {
+        if (err) throw err;
+
+        if (result.length > 0) {
+          var memberships = person.memberList();
+
+          if (memberships.length > 0) {
+            this.current_band(band.list.get(memberships[0].band_id()));
+          }
+        }
+      }.bind(this));
+    }.bind(this));
+  }.bind(this));
+
+  artist.load(function(err, result) {
+    if (err) throw err;
+  }.bind(this));
+  song.load(function(err, result) {
+    if (err) throw err;
+  }.bind(this));
+  band_song.load(function(err, result) {
+    if (err) throw err;
+  }.bind(this));
+  song_rating.load(function(err, result) {
+    if (err) throw err;
+  }.bind(this));
+  request.load.load(function(err, result) {
+    if (err) throw err;
+  }.bind(this));
+
+  this.reports.load();
 
   this.tab_list = ko.computed(function() {
     var result = tab_list = [
@@ -139,7 +215,7 @@ function Manager(for_test) {
       { value: 7, value_text: 'Band Songs' }
     ];
 
-    if (this.current_person().system_admin()) {
+    if (this.current_person() && this.current_person().system_admin()) {
       result.unshift({ value: 2, value_text: 'All People' });
       result.unshift({ value: 1, value_text: 'All Bands', });
     }
@@ -149,134 +225,73 @@ function Manager(for_test) {
     return result;
   }.bind(this));
 
-  if (!for_test) {
-    var svc = service.getInstance();
-    svc.get('./session_info', function(result_code, result) {
-      if (result_code != 200 && result_code != 304) {
-        throw new Error('Unexpected result ' + result_code);
-      }
-      
-      this.current_person(new Person(
-        result.person.id,
-        result.person.name,
-        result.person.full_name,
-        result.person.email,
-        result.person.system_admin
-      ));
-    }.bind(this));
-
-    this.bands.load();
-    this.band_members.load();
-
-    
-    this.persons.load();
-    this.artists.load();
-    this.songs.load();
-    this.band_songs.load();
-    this.song_ratings.load();
-    this.requests.load();
-    this.reports.load();
-  }
-
   this.current_tab = ko.observable(this.tab_list[0]);
 
   this.current_band_member = ko.computed(function() {
-    if (this.current_band()) {
-      var result = ko.utils.arrayFirst(this.band_members.list(), function(band_member) {
-        return band_member.band_id() == this.current_band().id() &&
-          band_member.person_id() == this.current_person().id();
-      }.bind(this));
-      return result || new BandMember();
+    if (this.current_band() && this.current_person()) {
+      var members = current_band().bandMemberList();
+      var result = ko.utils.arrayFirst(members, function(member_row) {
+        return member_row.person_id() == this.current_person.id();
+      }.bind(this))
+      return result;
     } else {
-      return new BandMember();
+      return null;
     }
-  }.bind(this)).extend({ throttle: 50 });
-
-  this.membership_sorts = ko.computed(function() {
-    return ko.utils.arrayFilter(this.band_members.sort_compare_labels, function(sort_compare) {
-      return sort_compare.value.match('band_name_');
-    }.bind(this))
-  }.bind(this));
-
-  this.current_memberships = ko.computed(function() {
-    return ko.utils.arrayFilter(this.band_members.filtered_list(), function(band_member) {
-      return band_member.person_id() == this.current_person().id();
-    }.bind(this));
   }.bind(this)).extend({ throttle: 50 });
 
   this.current_bands = ko.computed(function() {
     return ko.utils.arrayMap(
-      this.current_person().memberships(), 
-      function(band_member) { return band_member.band() }
+      this.current_person().bandMemberList(), 
+      function(member_row) { return member_row.band() }
     );
   }.bind(this)).extend({ throttle: 50 });
 
   this.other_bands = ko.computed(function() {
-    return ko.utils.arrayFilter(this.bands.list(), function(band) {
-      var member = ko.utils.arrayFirst(this.band_members.list(), function(band_member) {
-        return band_member.band_id() == band.id() &&
-          band_member.person_id() == this.current_person().id();
-      }.bind(this));
-      return !member;
+    return ko.utils.arrayFilter(this.bands.list(), function(band_row) {
+      if (this.current_person()) {
+        var filter = orm.table.list.filter.columnFilterFactory(
+          band_row.bandMemberList,
+          'equal',
+          'person_id'
+        );
+        filter.setFilterValue(this.current_person().id());
+        return filter.getList().length == 0;
+      } else {
+        return this.bands.list();
+      }
     }.bind(this));
   }.bind(this)).extend({ throttle: 50 });
 
-  this.band_member_sorts = ko.computed(function() {
-    return ko.utils.arrayFilter(this.band_members.sort_compare_labels, function(sort_compare) {
-      return sort_compare.value.match('person_') || sort_compare.value.match('band_admin_');
-    }.bind(this));
-  }.bind(this));
-
-  this.current_members = ko.computed(function() {
-    if (this.current_band()) {
-      return ko.utils.arrayFilter(this.band_members.filtered_list(), function(band_member) {
-        return band_member.band_id() == this.current_band().id();
-      }.bind(this));
-    } else {
-      return [];
-    }
-  }.bind(this)).extend({ throttle: 150 });
-
   this.non_band_members = ko.computed(function() {
     if (this.current_band()) {
-      return ko.utils.arrayFilter(this.persons.list(), function(person) {
-        var member = ko.utils.arrayFirst(this.band_members.list(), function(band_member) {
-          return band_member.band_id() == this.current_band().id() &&
-            band_member.person_id() == person.id();
-        }.bind(this));
-        return !member;
+      return ko.utils.arrayFilter(this.persons.list(), function(person_row) {
+        var filter = orm.table.list.filter.columnFilterFactory(
+          person_row.bandMemberList,
+          'equal',
+          'band_id'
+        );
+        filter.setFilterValue(this.current_band().id());
+        return filter.getList().length == 0;
       }.bind(this));
     } else {
       return this.persons.list();
-    }
-  }.bind(this)).extend({ throttle: 150 });
-
-  this.current_band_songs = ko.computed(function() {
-    if (this.current_band()) {
-      return ko.utils.arrayFilter(this.band_songs.filtered_list(), function(band_song) {
-        return band_song.band_id() == this.current_band().id();
-      }.bind(this));
-    } else {
-      return [];
     }
   }.bind(this)).extend({ throttle: 50 });
 
   this.non_band_songs = ko.computed(function() {
     if (this.current_band()) {
-      return ko.utils.arrayFilter(this.songs.list(), function(song) {
-        var result = ko.utils.arrayFirst(this.band_songs.list(), function(band_song) {
-          return band_song.band_id() == this.current_band().id() &&
-            band_song.song_id() == song.id();
-        }.bind(this));
-        return !result;
+      return ko.utils.arrayFilter(this.songs.list(), function(song_row) {
+        var filter = orm.table.list.filter.columnFilterFactory(
+          song_row.bandSongList,
+          'equal',
+          'band_id'
+        );
+        filter.setFilterValue(this.current_band().id());
+        return filter.getList().length == 0;
       }.bind(this));
     } else {
       return this.songs.list();
     }
-  }.bind(this)).extend({ throttle: 50 });
-
-  this.current_requests = ko.computed(function() {
-    return this.requests.filtered_list();
   }.bind(this)).extend({ throttle: 50 });
 
   this.current_reports = ko.computed(function() {
@@ -404,6 +419,124 @@ function Manager(for_test) {
     console.log('Test Click');
   };
 }
+
+Manager.prototype.createBandTable = function() {
+  var band = orm.define(
+    this,
+    'band',
+    {name: {type: 'string'}},
+    {
+      filters: [{
+        name: 'max_song_count',
+        type: 'max', 
+        column_name: 'bandSongCount'
+      }, {
+        name: 'min_song_count',
+        type: 'min', 
+        column_name: 'bandSongCount'
+      }, {
+        name: 'max_member_count',
+        type: 'max', 
+        column_name: 'bandMemberCount'
+      }, {
+        name: 'min_member_count',
+        type: 'min', 
+        column_name: 'bandMemberCount'
+      }, {
+        name: 'name',
+        type: 'match',
+        column_name: 'name'
+      }],
+      sort: [{
+        name: 'name_asc',
+        label: 'Name (Lo-Hi)',
+        definition: {name: 'asc'}
+      }, {
+        name: 'name_desc',
+        label: 'Name (Hi-Lo)',
+        definition: {name: 'desc'}
+      }, {
+        name: 'member_asc',
+        label: 'Member Count (Lo-Hi)',
+        definition: {bandMemberCount: 'asc'}
+      }, {
+        name: 'member_desc',
+        label: 'Member Count (Hi-Lo)',
+        definition: {bandMemberCount: 'desc'}
+      }, {
+        name: 'song_asc',
+        label: 'Song Count (Lo-Hi)',
+        definition: {bandSongCount: 'asc'}
+      }, {
+        name: 'song_desc',
+        label: 'Song Count (Hi-Lo)',
+        definition: {bandSongCount: 'desc'}
+      }]
+    }
+  );
+};
+
+Manager.prototype.createPersonTable = function() {
+  var person = orm.define(this, 'person', {
+    name: {type: 'string'},
+    full_name: {type: 'string'},
+    email: {type: 'string'},
+    system_admin: {type: 'boolean'}
+  }, {
+    filters: [{
+      name: 'max_band_count',
+      type: 'max',
+      column_name: 'bandCount'
+    }, {
+      name: 'min_band_count',
+      type: 'min',
+      column_name: 'bandCount'
+    }, {
+      name: 'system_admin',
+      type: 'bool',
+      column_name: 'system_admin'
+    }, {
+      name: 'email',
+      type: 'match',
+      column_name: 'email'
+    }, {
+      name: 'full_name',
+      type: 'match',
+      column_name: 'full_name'
+    }, {
+      name: 'name',
+      type: 'match',
+      column_name: 'name'
+    }],
+    sort: [{
+      name: 'name_asc',
+      label: 'Name (Lo-Hi)',
+      definition: {name: 'asc'}
+    }, {
+      name: 'name_desc',
+      label: 'Name (Hi-Lo)',
+      definition: {name: 'desc'}
+    }, {
+      name: 'full_name_asc',
+      label: 'Full Name (Lo-Hi)',
+      definition: {full_name: 'asc'}
+    }, {
+      name: 'full_name_desc',
+      label: 'Full Name (Lo-Hi)',
+      definition: {full_name: 'desc'}
+    }, {
+      name: 'email_asc',
+      label: 'E-Mail (Lo-Hi)',
+      definition: {email: 'asc'}
+    }, {
+      name: 'email_desc',
+      label: 'E-Mail (Lo-Hi)',
+      definition: {email: 'desc'}
+    }]
+  });
+
+
+};
 
 function app_start() {
   ko.applyBindings(new Manager());
